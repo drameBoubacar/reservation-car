6525490d51cfc24f3c4d43ddf457da6f6b6c2277
rayaneayd <raysayd23@gmail.com>
2022-03-11 16:24:06 +0100

    4:23 pm login db php

--- Login/connect.php
+++ Login/connect.php
@@ -5,7 +5,7 @@ $username = "root";
 $password = "";
 
 try {
-  $db = new PDO('mysql:host=localhost;dbname=YOUR_DATABASE_NAME;charset=utf8mb4', 'root', '');
+  $db = new PDO('mysql:host=$host;dbname=$db_name;charset=utf8mb4', 'root', '');
   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);  
 } catch (PDOException $e) {
