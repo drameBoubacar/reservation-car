1bc118a62cc22598c3df072cfc5605e197b95850
moha2507 <mohamedouali153@gmail.com>
2022-03-11 11:40:15 +0100

    fix menu

--- index.php
+++ index.php
@@ -44,7 +44,7 @@
                 <h1><span class="heading">A new</span> <span>experience.</span><br>Reserve your car<br>Now.</h1>
 
                 <div class="btn">
-                <input type="button" value="Démarrer">
+                <input type="button" id="btn1" value="Démarrer">
                
                 </div>
 
