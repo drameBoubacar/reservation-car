fd1314a90b6c9455240bad0cceec33ed21649100
Wacil23 <wacilzek00@gmail.com>
2022-03-17 00:28:46 +0100

    DATE FORM 00:28

--- main.js
+++ main.js
@@ -38,27 +38,27 @@ $(document).ready(function() {
     const map = new google.maps.Map(document.getElementById("map"), {
         center: { lat: 50.064192, lng: -130.605469 },
         zoom: 3,
-      });
-  
-      
-    
-      const center = { lat: 50.064192, lng: -130.605469 };
-      // Create a bounding box with sides ~10km away from the center point
-      const defaultBounds = {
+    });
+
+
+
+    const center = { lat: 50.064192, lng: -130.605469 };
+    // Create a bounding box with sides ~10km away from the center point
+    const defaultBounds = {
         north: center.lat + 0.1,
         south: center.lat - 0.1,
         east: center.lng + 0.1,
         west: center.lng - 0.1,
-      };
-      const input = document.getElementById("search_input");
-      const options = {
+    };
+    const input = document.getElementById("search_input");
+    const options = {
         bounds: defaultBounds,
         componentRestrictions: { country: "fr" },
         fields: ["address_components", "geometry", "icon", "name"],
         strictBounds: false,
         types: ["establishment"],
-      };
-      const autocomplete = new google.maps.places.Autocomplete(input, options);
+    };
+    const autocomplete = new google.maps.places.Autocomplete(input, options);
 
 });
 
@@ -186,33 +186,49 @@ function saveDate() {
 
     })
 }
+const deg = 6;
+const hour = document.querySelector(".hour");
+const min = document.querySelector(".min");
+const sec = document.querySelector(".sec");
 
-function showTime() {
-    var date = new Date();
-    var h = date.getHours(); // 0 - 23
-    var m = date.getMinutes(); // 0 - 59
-    var s = date.getSeconds(); // 0 - 59
-    var session = "AM";
+const setClock = () => {
+    let day = new Date();
+    let hh = day.getHours() * 30;
+    let mm = day.getMinutes() * deg;
+    let ss = day.getSeconds() * deg;
 
-    if (h == 0) {
-        h = 12;
+    hour.style.transform = `rotateZ(${hh + mm / 12}deg)`;
+    min.style.transform = `rotateZ(${mm}deg)`;
+    sec.style.transform = `rotateZ(${ss}deg)`;
+};
+
+// first time
+setClock();
+// Update every 1000 ms
+setInterval(setClock, 1000);
+
+const switchTheme = (evt) => {
+    const switchBtn = evt.target;
+    if (switchBtn.textContent.toLowerCase() === "light") {
+        switchBtn.textContent = "dark";
+        // localStorage.setItem("theme", "dark");
+        document.documentElement.setAttribute("data-theme", "dark");
+    } else {
+        switchBtn.textContent = "light";
+        // localStorage.setItem("theme", "light"); //add this
+        document.documentElement.setAttribute("data-theme", "light");
     }
+};
 
-    if (h > 12) {
-        h = h - 12;
-        session = "PM";
-    }
+const switchModeBtn = document.querySelector(".switch-btn");
+switchModeBtn.addEventListener("click", switchTheme, false);
 
-    h = (h < 10) ? "0" + h : h;
-    m = (m < 10) ? "0" + m : m;
-    s = (s < 10) ? "0" + s : s;
+let currentTheme = "dark";
+// currentTheme = localStorage.getItem("theme")
+// 	? localStorage.getItem("theme")
+// 	: null;
 
-    var time = h + ":" + m + ":" + s + " " + session;
-    document.getElementById("MyClockDisplay").innerText = time;
-    document.getElementById("MyClockDisplay").textContent = time;
-
-    setTimeout(showTime, 1000);
-
+if (currentTheme) {
+    document.documentElement.setAttribute("data-theme", currentTheme);
+    switchModeBtn.textContent = currentTheme;
 }
-
-showTime();
